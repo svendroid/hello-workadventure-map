c8c66943e0d426cc1bca784064786ad4a321ec1b
Sven Adolph <mail@svenadolph.net>
2021-02-10 21:48:52 +0100

    init

---
+++ export_imgs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/Applications/Tiled.app/Contents/MacOS/tmxrasterizer ./welcome.json ./map_images/welcome.png
+/Applications/Tiled.app/Contents/MacOS/tmxrasterizer ./office.json ./map_images/office.png
